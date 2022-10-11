e64307a6405857aa8fe102d3a8eebff277b4aaca
AntonSvirid <adsvirid@tut.by>
2022-10-11 11:45:40 +0300

    add someone...

--- src/by/itstep/antonsvirid/controller/Main.java
+++ src/by/itstep/antonsvirid/controller/Main.java
@@ -1,11 +1,12 @@
-package controller;
+package by.itstep.antonsvirid.controller;
 
 import by.itstep.antonsvirid.model.entity.Student;
+import by.itstep.antonsvirid.view.Printer;
 
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
 
-
+      //  Printer.print("testing");
     }
 }
---
+++ src/by/itstep/antonsvirid/view/Printer.java
@@ -0,0 +1,7 @@
+package by.itstep.antonsvirid.view;
+
+public class Printer {
+//    public static print(String msg) {
+//        System.out.println(msg);
+  //  }
+}
